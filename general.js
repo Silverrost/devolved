a279833973ed812019d3ff791e873bb6c4e4dcfa
Silverrost <tomujmejl@gmail.com>
2025-01-19 20:48:59 +0100

    modified

--- general.js
+++ general.js
@@ -1,6 +1,7 @@
 var Webflow = Webflow || [];
 Webflow.push(function() {
     // Fix dropdowns
+    /*
 	var drpdwns = document.querySelectorAll(".w-dropdown");
     for (var i = 0; i < drpdwns.length; i++) {
         var drpdwn = drpdwns[i]
@@ -8,10 +9,11 @@ Webflow.push(function() {
             drpdwn.classList.add("w--current");
 	    }
     }
-
+*/
     // reload on w-resized:
     var oldW = $(window).width();
-    console.log("oldW "+oldW);
+    console.log("oldW:");
+    console.log(oldW);
 	$(window).on('resize', function () {
 		if (window.resFin) clearTimeout(window.resFin);
         // resize end:
